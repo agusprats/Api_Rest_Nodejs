104b77290ed2a28fee251ee0b977374e8de674c0
aguspop <59830072+aguspop@users.noreply.github.com>
2020-07-09 17:19:56 +0100

    added methods update and delete

--- controllers/products.js
+++ controllers/products.js
@@ -14,5 +14,32 @@ module.exports = {
             'success': true,
             'msg': 'successfully added'
         });
-    }
-};
+    },
+    updateProduct: (req, res) => {
+        const { id } = req.params;
+        const { name } = req.body;
+
+        products.forEach((product, i) =>{
+            if(product.id === Number(id)){
+                product.name=name;
+            }
+        });
+        res.json({
+            'success': true,
+            'msg': 'updated'
+
+        })
+    },
+    deleteProduct: (req, res) => {
+        const { id } = req.params;
+        products.forEach((product, i) => {
+            if(product.id === Number(id)){
+                products.splice(i, 1);
+            }
+        });
+        res.json({
+            'success': true,
+            'msg': 'deleted'
+        })
+    },
+}
--- routes/products.js
+++ routes/products.js
@@ -2,12 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 //Para traer los Controllers
-const { getProducts, addProduct} = require('../controllers/products');
+const { 
+    getProducts, 
+    addProduct,
+    updateProduct,
+    deleteProduct
+} = require('../controllers/products');
 
 console.log(products);
 
 router.route("/")
     .get(getProducts)
     .post(addProduct);
+
+router.route('/:id')
+    .put(updateProduct)
+    .delete(deleteProduct)
     
 module.exports = router;
